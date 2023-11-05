820f5bb509bb32637293359deb8f18ea3986c7f6
sk337 <natethemakerreal@gmail.com>
2023-11-05 14:47:19 -0800

    add font awesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import startSparkle from "./sparkle";
 import Analytics from "analytics";
 import googleTagManager from "@analytics/google-tag-manager";
 
+import {library, dom} from "@fortawesome/fontawesome-svg-core";
+import {fab} from "@fortawesome/free-brands-svg-icons";
+import {far} from "@fortawesome/free-regular-svg-icons";
+import {fas} from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, far, fas);
+
+dom.i2svg();
+
 startSparkle();
 
 const analytics = Analytics({
