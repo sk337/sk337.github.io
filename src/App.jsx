d557fa5b09b991db2704051abde13c317229e28f
sk337 <nathan@shorthoppers.com>
2024-01-23 20:24:24 -0800

    add webpage

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="flex items-center justify-center">
+      <div className="w-4/5">
+        <p className=" w-full text-center font-bold text-3xl m-5">
+          Hello, I am sk337
+        </p>
+        <p>Projects:</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
